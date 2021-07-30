67b2ab677f7135e41acb52d59c4fe3978a3bd9f8
super-rogatory <chukwudi10@gmail.com>
2021-07-30 01:13:27 -0700

    Can authenticate correctly

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -2,14 +2,33 @@ const express = require("express");
 const router = express.Router();
 const utils = require("../../libs/utils");
 const User = require("../db/User");
-
+const passport = require("passport")
 // Mounted on '/api/users
 // router.get("/", (req, res, next) => {
 //   res.send({ msg: "hello world" });
 // });
 
-router.get("/protected", (req, res, next) => {});
-router.get("/login", (req, res, next) => {});
+router.post("/protected", passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.status(200).json({msg: "access granted"});
+});
+
+router.post("/login", async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { name: req.body.username }});
+        if(!user) res.status(401).json( {msg: "could not find user"} );
+        const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+
+        if(isValid) {
+            const tokenObject = utils.issueJWT(user);
+            res.status(200).json({ user, token: tokenObject.token, expiresIn: tokenObject.expiresIn })
+        } else {
+            res.status(401).json({ msg: "You have entered the wrong password "});
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
 router.post("/register", async (req, res, next) => {
   try {
     // if a user comes to the application and register, we are going to store that new user in the database and issue them a new jwt
